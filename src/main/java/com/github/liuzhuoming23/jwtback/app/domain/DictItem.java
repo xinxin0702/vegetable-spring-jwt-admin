189a2cbe227cb01dc97497daf54b9b4099d5c479
liuzhuoming <liuzhuoming23@live.com>
2019-05-24 22:53:15 +0800

    修改字典内容值限制

--- src/main/java/com/github/liuzhuoming23/jwtback/app/domain/DictItem.java
+++ src/main/java/com/github/liuzhuoming23/jwtback/app/domain/DictItem.java
@@ -1,5 +1,7 @@
 package com.github.liuzhuoming23.jwtback.app.domain;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -19,6 +21,8 @@ public class DictItem {
     @Size(min = 1, max = 20)
     private String context;
     @NotNull
+    @Min(0)
+    @Max(999)
     private Integer val;
     private Integer sort;
 }
